8502fb3e049a40a7bb116152a009131b0a5f5e2b
Jamie <jrs596@york.ac.uk>
2022-06-22 10:51:13 +0100

    .

--- run.sh
+++ run.sh
@@ -36,7 +36,7 @@ export DATA_DIR='/jmain02/home/J2AD016/jjw02/jjs00-jjw02/dat/ResNetFung50+_image
 export CHECKPOINT_DIR='/jmain02/home/J2AD016/jjw02/jjs00-jjw02/dat/NVAE/CheckPoint'       #PATH_TO_CHECKPOINT_DIR
 export CODE_DIR='/jmain02/home/J2AD016/jjw02/jjs00-jjw02/scripts/NVAE'        #PATH_TO_CODE_DIR
 cd $CODE_DIR
-python train.py --data $DATA_DIR --root $CHECKPOINT_DIR --save $EXPR_ID --dataset custom --batch_size 32 \
+python train.py --data $DATA_DIR --root $CHECKPOINT_DIR --save $EXPR_ID --dataset custom --batch_size 42 \
         --input_size 256 --epochs 400 --num_latent_scales 1 --num_groups_per_scale 2 --num_postprocess_cells 3 \
         --num_preprocess_cells 3 --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 --num_latent_per_group 2 \
         --num_preprocess_blocks 2 \
